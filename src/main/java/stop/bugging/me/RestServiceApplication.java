a6dc3a6632c24b6fc3d6bc1c61670b9fb835c241
Jessica Kerr <jessitron@gmail.com>
2017-08-07 18:07:55 -0700

    initial commit by Atomist

---
+++ src/main/java/stop/bugging/me/RestServiceApplication.java
@@ -0,0 +1,12 @@
+package stop.bugging.me;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RestServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(RestServiceApplication.class, args);
+    }
+}
---
+++ src/test/java/stop/bugging/me/RestServiceApplicationTests.java
@@ -0,0 +1,15 @@
+package stop.bugging.me;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class RestServiceApplicationTests {
+
+    @Test
+    public void contextLoads() {
+    }
+}
